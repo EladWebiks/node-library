313b7596cdf25a9c459eadcc2cd46936f7853132
EladWebiks <elad_harel@webiks.com>
2024-09-25 16:33:57 +0300

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import authRouter from "./routes/auth.js";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+app.use(cors());
 app.use(express.json()); // Body parser
 app.use("/", authRouter); // Auth Router
 app.listen(PORT, () => {
--- server.ts
+++ server.ts
@@ -1,10 +1,12 @@
 import express, { Application } from "express";
 import authRouter from "./routes/auth.js";
 import dotenv from "dotenv";
-
+import cors from "cors"
 dotenv.config();
 const PORT: number | string = process.env.PORT || 3000;
 const app: Application = express();
+
+app.use(cors())
 
 app.use(express.json()); // Body parser
 
